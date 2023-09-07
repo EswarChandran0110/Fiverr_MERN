03cf0a90e548e22c744460420b6b5ea07712ee3e
Eswar_Chandran <eswarchandran0110@gmail.com>
2023-09-07 09:27:32 +0530

    gigPage completed

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -3,7 +3,7 @@ import "./GigCard.scss"
 
 function GigCard({item}) {
   return (
-    <Link to="/gig/123">
+    <Link to="/gig/123" className="link">
     <div className="gigCard">
         <img src={item.img} alt="" />
         <div className="info">
@@ -17,8 +17,9 @@ function GigCard({item}) {
                 <span>{item.star}</span>
             </div>
         </div>
+        <hr />
         <div className="details">
-            <img src="" alt="" />
+            <img src="./img/heart.png" alt="" />
             <div className="price">
               <span>Starting at</span>
               <h2>${item.price}</h2>
